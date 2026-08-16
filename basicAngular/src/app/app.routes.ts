c3fdf625a98be53e652cd79ee4d147899fa52a6b
Chandan Kumar <93089403+chandan578@users.noreply.github.com>
2026-08-16 18:12:45 +0530

    Practice some control flow statements @if and @for

--- basicAngular/src/app/app.routes.ts
+++ basicAngular/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Veriables } from './components/veriables/veriables';
 import { DataBinding } from './components/data-binding/data-binding';
 import { DirectiveExp } from './components/directive-exp/directive-exp';
 import { NotFound } from './components/not-found/not-found';
+import { ControlFlow } from './components/control-flow/control-flow';
 
 export const routes: Routes = [
     {
@@ -21,6 +22,10 @@ export const routes: Routes = [
     {
         path: 'directives',
         component: DirectiveExp
+    },
+    {
+        path: 'controlflow',
+        component: ControlFlow
     },
     {
         path: '**',
---
+++ basicAngular/src/app/components/control-flow/control-flow.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-control-flow',
+  imports: [FormsModule],
+  templateUrl: './control-flow.html',
+  styleUrl: './control-flow.css',
+})
+export class ControlFlow {
+
+  isToggleDiv: boolean = true;
+  isOffer: boolean = true;
+  orderStatus: string = 'failed';
+
+  cityList = ['Delhi', "Banglore", "Pune", "Patna", "Mumbai"]
+
+  isToggleDiv1() {
+    this.isToggleDiv = !this.isToggleDiv;
+  }
+
+  studentList = [
+    {studentId: "ab67", name: 'John', age: 20, city: 'New York', rollNo: 101, course: 'Mathematics' },
+    {studentId: "cd89", name: 'Alice', age: 22, city: 'Los Angeles', rollNo: 102, course: 'Physics' },
+    {studentId: "ef01", name: 'Bob', age: 19, city: 'Chicago', rollNo: 103, course: 'Chemistry' },
+    {studentId: "gh23", name: 'Eve', age: 21, city: 'Houston', rollNo: 104, course: 'Biology' },
+    {studentId: "ij45", name: 'Charlie', age: 23, city: 'San Francisco', rollNo: 105, course: 'Computer Science' },
+    {studentId: "kl67", name: 'David', age: 20, city: 'Boston', rollNo: 106, course: 'Engineering' },
+    {studentId: "mn89", name: 'Frank', age: 22, city: 'Seattle', rollNo: 107, course: 'History' },
+    {studentId: "op01", name: 'Grace', age: 19, city: 'Miami', rollNo: 108, course: 'Art' },
+    {studentId: "qr23", name: 'Hannah', age: 21, city: 'Denver', rollNo: 109, course: 'Music' },
+    {studentId: "st45", name: 'Ian', age: 23, city: 'Atlanta', rollNo: 110, course: 'Literature' }
+  ];
+
+  selectedStudent = "Select a student";
+
+}
